d6c5d8ef7cf665d0a3448e341f65670d069b5ca6
hatrungdung <hatrungdung1982@gmail.com>
2013-04-18 16:13:54 +0700

    tweets init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweets::Application.config.secret_token = '532a347983a58dc467ba2b01bdf41a476c5ce588a93e1a64d45ec2e2a4baaa99b59ca3119a3a1a672afa4601fc9a95da27c0849225e1d57a45200d5ae9ffa269'
